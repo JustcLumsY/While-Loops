b855993a48300240946f59af4a8815907ec7c05c
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-12 09:46:52 +0100

    Add project files.

---
+++ While Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int index = 1;
+            //while (index <= 5)
+            //{
+            //    Console.WriteLine(index);
+            //    index++;
+            //}
+
+            int index = 6;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            while (index <= 5) ;
+        }
+    }
+}
